81d8b01e8626b0f752866c5e46cb54bd22433470
Eugene <eugene.fedor@gmail.com>
2021-06-28 12:36:51 -0700

    smoke test

--- utils/smoke-test.sh
+++ utils/smoke-test.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # Simple smoke test requesting demo app url and failing in case of error response
-# Usage smoke-test.sh DEMO_APP_URL
 
-curl -v --fail $1
+set -x
+
+az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_NAME
+
+kubectl port-forward svc/azure-vote-front 8080:80 --namespace=$TARGET_NAMESPACE
+portForwardPid=$!
+
+curl -v --fail http://127.0.0.1:8080
+
+kill $portForwardPid
