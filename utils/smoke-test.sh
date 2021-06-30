f58f8e5796a541a92d60c5824c8826c7698ce7b2
Eugene <eugene.fedor@gmail.com>
2021-06-29 19:40:41 -0700

    smoke test

--- utils/smoke-test.sh
+++ utils/smoke-test.sh
@@ -6,9 +6,9 @@ set -x
 
 az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_NAME
 
-kubectl port-forward svc/azure-vote-front 8080:80 --namespace=$TARGET_NAMESPACE&
+kubectl port-forward svc/azure-vote-front 5678:80 --namespace=$TARGET_NAMESPACE&
 portForwardPid=$!
 
-curl -v --fail http://127.0.0.1:8080
+curl -v --fail http://127.0.0.1:5678
 
 kill $portForwardPid
